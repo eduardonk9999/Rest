a92692a00bd37c64b8ba3329cf9dd977d3f3d8a1
Eduardo <thiago.novak7@gmail.com>
2020-08-31 21:09:30 -0300

    Add loops

--- wp-content/themes/rest/index.php
+++ wp-content/themes/rest/index.php
@@ -1,4 +1,12 @@
 
 <?php get_header(); ?>
+    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+        <?php the_title(); ?>
+        <?php the_content(); ?>
 
+    <?php endwhile; else: ?>
+        <p><?php _e('Não tem post'); ?></p>
+    <?php endif; ?>
+    
+            
 <?php get_footer(); ?>
